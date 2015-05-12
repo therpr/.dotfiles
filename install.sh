2045e51fbfd5b2c2b2a86f698fd9b27406bf05ea
Sebastian Witkowski <switkowski@Szymons-MacBook-Pro-5.local>
2015-05-12 13:04:02 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ln -sfv "~/.dotfiles/vim/.vimrc" ~
+ln -sfv "~/.dotfiles/idea/.ideavimrc" ~
+ln -sfv "~/.dotfiles/tmux/.tmux.conf" ~
+ln -sfv "~/.dotfiles/git/.gitconfig" ~
+ln -sfv "~/.dotfiles/system/.zshrc" ~
+ln -sfv "~/.dotfiles/system/.aliases" ~
+ln -sfv "~/.dotfiles/system/.exports" ~
+ln -sfv "~/.dotfiles/system/.functions" ~
+
